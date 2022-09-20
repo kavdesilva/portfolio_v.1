b7a9678e389f49ccdeb40f63ca962ccbb32c9025
Kristina <kavdesilva@gmail.com>
2022-09-20 00:29:11 -0400

    added screenshots for all projects. just need to include link for resume, then ready to deploy.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,7 +8,7 @@ const About = ({ aboutRef }) => {
                 <div className="about-me">
                     <h2>hello! 🌻</h2>
                     <img className="profile-image" src={Kristina} alt="profile image" />
-                    <h2>my name is kristina.</h2>
+                    <h2>my name is kristina.</h2><br/>
                     <p>I am a software engineer interested in building high quality products that are beautiful, functional and easy to use. I LOVE to learn and I am not afraid to take on a challenge–-there is nothing more satisfying than a hard-won solution to a difficult problem!</p><br/>
                 </div>
             </section>
--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,4 +1,7 @@
 import Moonsweepr from '../assets/moonsweepr_screenshot_1.png'
+import AuthorWebsite from '../assets/author_website_screenshot.png'
+import CycleList from '../assets/CycleList_Screenshot.png'
+import Hekate from '../assets/hekate_screenshot.png'
 
 const Portfolio = ({ portfolioRef }) => {
     return (
@@ -9,25 +12,33 @@ const Portfolio = ({ portfolioRef }) => {
                     <div className="portfolio-card">
                         <a href="http://moonsweepr.surge.sh/" target="_blank">
                             <img src={Moonsweepr} alt="moonsweepr screenshot" className='portfolio-image'/>
-                            <p>moonsweepr</p>
+                            <h4>moonsweepr</h4>
+                            <p>a space-themed minesweeper game.</p>
+                            <p>HTML, CSS, JS</p>
                         </a>
                     </div>
                     <div className="portfolio-card">
                         <a href="https://github.com/kavdesilva/author_website" target="_blank">
-                            <img src="" alt="author website template screenshot" className='portfolio-image'/>
-                            <p>author website (template)</p>
+                            <img src={AuthorWebsite} alt="author website template screenshot" className='portfolio-image'/>
+                            <h4>author website (template)</h4>
+                            <p>an interactive website for an author's collection of works.</p>
+                            <p>MongoDB, Express, Node, React</p>
                         </a>
                     </div>
                     <div className="portfolio-card">
                         <a href="https://github.com/frank-booth/CycleList_Frontend" target="_blank">
-                            <img src="" alt="cyclelist screenshot" className='portfolio-image'/>
-                            <p>CycleList</p>
+                            <img src={CycleList} alt="cyclelist screenshot" className='portfolio-image'/>
+                            <h4>CycleList</h4>
+                            <p>a web application for creating cycle routines to your favorite tunes.</p>
+                            <p>PostgreSQL, Express, Node, React</p>
                         </a>
                     </div>
                     <div className="portfolio-card">
                         <a href="https://github.com/kavdesilva/Hekate" target="_blank">
-                            <img src="" alt="hekate screenshot" className='portfolio-image'/>
-                            <p>hekate</p>
+                            <img src={Hekate} alt="hekate screenshot" className='portfolio-image'/>
+                            <h4>hekate</h4>
+                            <p>a period tracking app focused on gender-neutrality and data security.</p>
+                            <p>PostgreSQL, Express, Node, Vue</p>
                         </a>
                     </div>
                 </div>
