4fdd121366140bfa38916b71a045c125aa3a8319
Kristina <kavdesilva@gmail.com>
2022-09-21 21:29:58 -0400

    portfolio section mobile responsive.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,7 +7,7 @@ const About = ({ aboutRef }) => {
             <section>
                 <div className="about-me">
                     <h2>hello! 🌻</h2>
-                    <img className="profile-image" src={Kristina} alt="profile image" />
+                    <img className="about-me-image" src={Kristina} alt="about-me-image" />
                     <h2>my name is kristina.</h2><br/>
                     <p>I am a software engineer interested in building high quality products that are beautiful, functional and easy to use. I LOVE to learn and I am not afraid to take on a challenge–-there is nothing more satisfying than a hard-won solution to a difficult problem!</p><br/>
                 </div>
